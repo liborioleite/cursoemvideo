ae6ea0d060e956cd1a9f614aab53ef585223af28
Joaquim Libório Leite Filho <joaquim.filho@ASSEMBLEIA.LOCAL>
2022-09-19 19:23:56 -0300

    adicionado arquivo de foto dinamicamente e mudanças no css e js

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-let horadia = document.getElementById('resultado1')
+let horadia = document.getElementById('conteudo1')
+horadia = new Date().getHours()
+document.getElementById('conteudo1').innerHTML = `São ${horadia} horas, está de noite!`
 
-horadia = new Date()
+
+if (horadia <= 12) {
+   document.getElementById('fotos').src = "/dia.jpg"
+    
+} else if (horadia <= 18) {
+    document.getElementById('fotos').src = "/tarde.jpg"
+} else if (horadia <= 23) {
+    document.getElementById('fotos').src = "/noite.jpg"
+}
